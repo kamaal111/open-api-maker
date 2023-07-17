3f555fa66ab87f8927e6fe52575cb4ee5b86db9a
Kamaal Farah <kamaal.f1@gmail.com>
2023-07-17 22:33:31 +0200

    Changing import paths

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from typing import TYPE_CHECKING, Any
 from pathlib import Path
 
 if TYPE_CHECKING:
-    from src.swagger_types import (
+    from swagger_types import (
         SwaggerDict,
         SwaggerPathMethod,
         SwaggerResponse,
